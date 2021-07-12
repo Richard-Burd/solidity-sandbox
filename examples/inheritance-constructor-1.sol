d1e0e5912410a6c6401c6fc5d7988c31540db43e
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-07-12 16:18:38 -0700

    inheritance-constructor-2 added

--- examples/inheritance-constructor-1.sol
+++ examples/inheritance-constructor-1.sol
@@ -1,31 +1,32 @@
 pragma solidity ^0.5.3;
 
 contract X {
-  event Log(string message);
   string public name;
+
   constructor(string memory _name) public {
     name = _name;
-    emit Log(_name);
   }
 }
 
 contract Y {
-  event Log(string message);
   string public text;
   constructor(string memory _text) public {
     text = _text;
-    emit Log(_text);
   }
 }
 
-contract E is X, Y {
-  constructor() X("X called from E") Y("Y called from E") public {
+contract B is X("Fixed input"), Y("Another fixed input") {
+
+}
+
+contract C is X, Y {
+  constructor() X("Fixed input") Y("Another fixed input") public {
 
   }
 }
 
-contract F is X, Y {
-  constructor() Y("Y called from F") X("X called from F") public {
+contract D is X, Y {
+  constructor(string memory _name, string memory _text) X(_name) Y(_text) public {
 
   }
 }
