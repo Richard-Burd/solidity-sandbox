646865981c1e8757294e94b429a8b2a79757d252
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-08-08 11:31:48 -0700

    fallback code example added

--- examples/fallback.sol
+++ examples/fallback.sol
@@ -6,4 +6,19 @@ contract Fallback {
   function() external payable {
     emit Log(gasleft());
   }
+
+  function getBalance() public view returns (uint) {
+    return address(this).balance;
+  }
+}
+
+contract SendToFallback {
+  function transferToFallback(address payable _to) public payable {
+    _to.transfer(msg.value);
+  }
+
+  function callFallback(address payable _to) public payable {
+    (bool sent,) = _to.call.value(msg.value)("");
+    require(sent, "Failed to send Ether");
+  }
 }
