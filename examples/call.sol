489e4c560a52cde8efed66202deac7484c18d539
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-08-08 12:10:33 -0700

    call code example completed

--- examples/call.sol
+++ examples/call.sol
@@ -16,5 +16,22 @@ contract Reciever {
 }
 
 contract Caller {
+  event Response(bool success, bytes data);
 
+  function testCallFoo(address payable _addr) public payable {
+    (bool success, bytes memory data) = _addr.call.value(msg.value).gas(5000)(
+      abi.encodeWithSignature("foo(string,uint256)", "call foo", 123)
+    );
+
+    emit Response(success, data);
+  }
+
+
+  function testCallDoesNotExist(address _addr) public {
+    (bool success, bytes memory data) = _addr.call(
+      abi.encodeWithSignature("doesNotExist()")
+    );
+
+    emit Response(success, data);
+  }
 }
