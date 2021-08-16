30e88c0c243c93de9c2c87803df6f63a97ddb754
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-08-16 13:34:58 -0700

    depricated call method shown w/new call method

--- examples/call-old.sol
+++ examples/call-old.sol
@@ -19,6 +19,8 @@ contract Caller {
   event Response(bool success, bytes data);
 
   function testCallFoo(address payable _addr) public payable {
+
+    // deprecated syntax                    ".call.value(msg.value).gas(5000)"
     (bool success, bytes memory data) = _addr.call.value(msg.value).gas(5000)(
       abi.encodeWithSignature("foo(string,uint256)", "call foo", 123)
     );
