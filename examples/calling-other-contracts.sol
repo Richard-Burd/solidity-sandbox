2e969c720148d3ba6fe9903d802e23213965f95a
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-08-19 09:24:04 -0700

    caller & foo contracts added

--- examples/calling-other-contracts.sol
+++ examples/calling-other-contracts.sol
@@ -16,3 +16,27 @@ contract Callee {
     return (x, value);
   }
 }
+
+contract Caller {
+  function setX(Callee _callee, uint _x) public {
+      uint x = _callee.setX(_x);
+  }
+
+  function setXFromAddress(address _addr, uint _x) public {
+    Callee callee = Callee(_addr);
+    uint x = callee.setX(_x);
+  }
+
+  function setXAndSendEther(Callee _callee, uint _x) public payable {
+    (uint x, uint value) = _callee.setXAndSendEther.value(msg.value)(_x);
+  }
+}
+
+contract Foo {
+  uint public x;
+
+  function setX(uint _x) public returns (uint) {
+    x = _x + 1;
+    return 0;
+  }
+}
